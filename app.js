e7b8af18e571ae8b8a3b306be551e6a6d283d19a
Fernando Gargano <fernando@google.com>
2018-08-23 13:00:57 +0200

    Added jade main layout and jade views for home and chatrooms views

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set("views", "./views");
+
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
-app.get('/hello', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function (req, res) {
+    res.render("index.jade");
+});
+
+app.get('/admin/rooms', function (req, res) {
+    res.render("rooms.jade");
 });
 
 app.listen(3000, 'localhost', function() {
