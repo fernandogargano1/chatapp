f227db421cf2f66dbad27c1e15e5d0fda2c101cb
Fernando Gargano <fernando@google.com>
2018-08-25 15:34:28 +0200

    Injecting data table in Jade view and loading JSON file inside of the app

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
+var rooms = require('./data/rooms.json');
+
 app.set("views", "./views");
+app.set("view engine", "jade");
 
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
 app.get('/', function (req, res) {
-    res.render("index.jade");
+    res.render("index", { title: "Home"}, function(err, html) {
+        console.log(html);  
+        
+        return res.send(html);
+    });
 });
 
-app.get('/admin/rooms', function (req, res) {
-    res.render("rooms.jade");
+app.get('/admin/rooms', function (req, res) {    
+    res.render("rooms", { 
+        title: "Admin Rooms",
+        rooms: rooms
+    });
 });
 
 app.listen(3000, 'localhost', function() {
