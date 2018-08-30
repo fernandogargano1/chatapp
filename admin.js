f0b4561ede0389471bed6263ccb714fe89b47f13
Fernando Gargano <fernando@google.com>
2018-08-30 12:52:17 +0200

    Grouped admin room handlers into admin module, used request baseUrl instead of handcoded absolute paths and route method on router to define common routes

---
+++ admin.js
@@ -0,0 +1,130 @@
+// We get back function to create random identifiers
+const uuidv4 = require('uuid/v4');
+const _ = require("lodash");
+
+var express = require('express');
+
+var rooms = require('./data/rooms.json');
+
+var router = express.Router();
+
+module.exports = router;
+
+
+router.get('/rooms', function (req, res) { 
+
+    res.render("rooms", { 
+        title: "Admin Rooms",
+        rooms: rooms,
+        baseUrl: req.baseUrl
+    });
+});
+
+// router.get('/rooms/add', function (req, res) {    
+//     res.render("add", { baseUrl: req.baseUrl });
+// });
+
+// router.post('/rooms/add', function (req, res) { 
+//     const chatroom = {
+//         name: req.body.name,
+//         id: uuidv4()
+//     }     
+
+//     rooms.push(chatroom);
+    
+//     // res.send() would have had the same effect
+//     // res.json(chatroom);
+//     // res.redirect('/admin/rooms');
+//     res.redirect(req.baseUrl + "/rooms");
+// });
+
+router.route('/rooms/add')
+    .get(function (req, res) {    
+        res.render("add", { baseUrl: req.baseUrl });
+    })
+
+    .post(function (req, res) { 
+        const chatroom = {
+            name: req.body.name,
+            id: uuidv4()
+        }     
+
+        rooms.push(chatroom);
+    
+    // res.send() would have had the same effect
+    // res.json(chatroom);
+    // res.redirect('/admin/rooms');
+        res.redirect(req.baseUrl + "/rooms");
+    });
+
+// router.get('/rooms/edit/:id', function(req, res) {
+//     const roomId = req.params.id;    
+
+//     // Get the room we want to edit.
+//     const room = _.find(rooms, r => r.id === roomId);
+//     if (!room) {
+//         res.sendStatus(404);
+//         return;        
+//     }
+
+//     // Pass this room to the view
+//     res.render('edit', { room, baseUrl: req.baseUrl });
+// });
+
+// router.post('/rooms/edit/:id', function (req, res) { 
+//     const roomId = req.params.id;    
+
+//     // Get the room we want to edit.
+//     const room = _.find(rooms, r => r.id === roomId);
+//     if (!room) {
+//         res.sendStatus(404);
+//         return;        
+//     }
+
+//     room.name = req.body.name;  
+    
+//     // res.redirect('/admin/rooms');
+//     res.redirect(req.baseUrl + "/rooms");
+//     // res.redirect("./");
+// });
+
+router.route('/rooms/edit/:id')
+    .get(function(req, res) {
+        const roomId = req.params.id;    
+
+        // Get the room we want to edit.
+        const room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;        
+        }
+
+        // Pass this room to the view
+        res.render('edit', { room, baseUrl: req.baseUrl });
+    })
+    .post(function (req, res) { 
+        const roomId = req.params.id;    
+
+        // Get the room we want to edit.
+        const room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;        
+        }
+
+        room.name = req.body.name;  
+        
+        // res.redirect('/admin/rooms');
+        res.redirect(req.baseUrl + "/rooms");
+        // res.redirect("./");
+    });
+
+router.get('/rooms/delete/:id', function(req, res) {
+    const roomId = req.params.id;
+
+    rooms = rooms.filter(r => r.id !== roomId);
+    //res.redirect('/admin/rooms');
+    res.redirect(req.baseUrl + "/rooms");
+});
+
+
--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
-// We get back function to create random identifiers
-const uuidv4 = require('uuid/v4');
-const _ = require("lodash");
-
-var rooms = require('./data/rooms.json');
 
 app.set("views", "./views");
 app.set("view engine", "jade");
@@ -19,67 +14,8 @@ app.get('/', function (req, res) {
     res.render("index", { title: "Home"});
 });
 
-app.get('/admin/rooms', function (req, res) { 
-    
-    res.render("rooms", { 
-        title: "Admin Rooms",
-        rooms: rooms
-    });
-});
-
-app.get('/admin/rooms/add', function (req, res) {    
-    res.render("add");
-});
-
-app.post('/admin/rooms/add', function (req, res) { 
-    const chatroom = {
-        name: req.body.name,
-        id: uuidv4()
-    }     
-
-    rooms.push(chatroom);
-    
-    // res.send() would have had the same effect
-    // res.json(chatroom);
-    res.redirect('/admin/rooms');
-});
-
-app.get('/admin/rooms/edit/:id', function(req, res) {
-    const roomId = req.params.id;    
-
-    // Get the room we want to edit.
-    const room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;        
-    }
-
-    // Pass this room to the view
-    res.render('edit', { room });
-});
-
-app.post('/admin/rooms/edit/:id', function (req, res) { 
-    const roomId = req.params.id;    
-
-    // Get the room we want to edit.
-    const room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;        
-    }
-
-    room.name = req.body.name;  
-    
-    res.redirect('/admin/rooms');
-});
-
-app.get('/admin/rooms/delete/:id', function(req, res) {
-    const roomId = req.params.id;
-
-    rooms = rooms.filter(r => r.id !== roomId);
-    res.redirect('/admin/rooms');
-});
-
+var adminRouter = require('./admin');
+app.use('/admin',adminRouter);
 
 app.listen(3000, 'localhost', function() {
     console.log('Chat app listening on port 3000');
